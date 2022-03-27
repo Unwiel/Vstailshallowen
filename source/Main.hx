cdd33aa9380a993cb53e4de33315db577724c694
Erickoxd <93102304+Unwiel@users.noreply.github.com>
2022-03-27 16:36:18 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -81,7 +81,7 @@ class Main extends Sprite
 		#end
 
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-                ;
+                
 		addChild(game);
 
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
